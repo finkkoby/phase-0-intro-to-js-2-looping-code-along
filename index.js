97ef51d3cd62e3da05f777f19f65c97729e7daa3
Koby Fink <finkkoby@icloud.com>
2024-01-19 08:27:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const cardMessages = [""];
+function writeCards(name, occasion = "surprise") {
+    for (let i = 0; i < name.length; i++) {
+        cardMessages[i] = `Thank you, ${name[i]}, for the wonderful ${occasion} gift!`;
+    }
+    return cardMessages;
+    console.log(cardMessages);
+}   
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
