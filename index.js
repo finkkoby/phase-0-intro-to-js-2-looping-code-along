dc99858de77a0cbdea48fe174bdbc2b401ea38a4
Koby Fink <finkkoby@icloud.com>
2024-02-01 09:09:01 -0500

    complete lab for phase 1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Code your solutions in this file
-const cardMessages = [""];
-function writeCards(name, occasion = "surprise") {
-    for (let i = 0; i < name.length; i++) {
-        cardMessages[i] = `Thank you, ${name[i]}, for the wonderful ${occasion} gift!`;
+function writeCards(array, event) {
+    const newArray = []
+    for(let name of array) {
+        newArray.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
     }
-    return cardMessages;
-    console.log(cardMessages);
-}   
-function countDown(number) {
-    while (number >= 0) {
-        console.log(number);
-        number--;
+    return newArray;
+}
+
+function countDown(int) {
+    while(int >= 0) {
+        console.log(int);
+        int--;
     }
 }
